edcac737ae7b7db823f5c7bcafb25ae521f13da2
Yinyin L <yinyinl@gmail.com>
2024-06-16 01:13:15 +0800

    fix: count delimiter in

--- kebebcase.go
+++ kebebcase.go
@@ -6,7 +6,7 @@ import (
 
 func ToKebebCase(name string, opts *Options) string {
 	nameParts := Split(name, opts)
-	resultBuf := make([]byte, 0, computeBufferSize(nameParts, false))
+	resultBuf := make([]byte, 0, computeBufferSize(nameParts, true))
 	for i, part := range nameParts {
 		part = strings.ToLower(part)
 		if i != 0 {
