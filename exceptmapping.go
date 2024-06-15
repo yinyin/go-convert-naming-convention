bbadfe097ac453bdb067e1df706c2b641eed2590
Yinyin L <yinyinl@gmail.com>
2024-06-16 01:05:29 +0800

    fix: check long match first

--- exceptmapping.go
+++ exceptmapping.go
@@ -2,11 +2,38 @@ package convertnamingconvention
 
 import (
 	"slices"
+	"sort"
 )
 
 type exceptRule struct {
 	toMatch     []string
 	replaceWith []string
+}
+
+type byToMatch []exceptRule
+
+func (a byToMatch) Len() int      { return len(a) }
+func (a byToMatch) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a byToMatch) Less(i, j int) bool {
+	toMatchLenI := len(a[i].toMatch)
+	toMatchLenJ := len(a[j].toMatch)
+	if toMatchLenI > toMatchLenJ {
+		return true
+	}
+	if toMatchLenI < toMatchLenJ {
+		return false
+	}
+	for idx := 0; idx < toMatchLenI; idx++ {
+		if len(a[i].toMatch[idx]) < len(a[j].toMatch[idx]) {
+			return true
+		}
+	}
+	for idx := 0; idx < toMatchLenI; idx++ {
+		if a[i].toMatch[idx] < a[j].toMatch[idx] {
+			return true
+		}
+	}
+	return false
 }
 
 func toExceptRules(rawRule map[string]string, commonInitialisms map[string]bool, splitAlphaNum bool) (exceptRules []exceptRule) {
@@ -16,6 +43,7 @@ func toExceptRules(rawRule map[string]string, commonInitialisms map[string]bool,
 			replaceWith: splitNameWithDelimiter(replaceWith, commonInitialisms),
 		})
 	}
+	sort.Sort(byToMatch(exceptRules))
 	return
 }
 
